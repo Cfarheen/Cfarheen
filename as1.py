1d300f71b20a9f8f08628a9dd1039b6c1963011f
Farheen <farheenchowdhary1504@gmail.com>
2023-11-06 22:37:23 +0530

    first commit

--- as1.py
+++ as1.py
@@ -1,10 +1,3 @@
-"""
-Module Docstring: A short description of the module's purpose.
-"""
-
-# Assign values to variables A and B
 A = 5
 B = 10
-
-# Calculate and print the sum of A and B
 print(A + B)
